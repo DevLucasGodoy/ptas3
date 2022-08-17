7391379c10958a033e86f528906d3d6cce02b410
Lucas Godoy <lucas.moraes.godoy@gmail.com>
2022-08-17 14:09:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
 
 app.get('/', async function(req, res){
   // var resultado = await usuario.findAll();
