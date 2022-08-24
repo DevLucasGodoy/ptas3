14a11fc4df5c9ca57cdc4b8c9efc9bcc588aff5d
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-08-24 14:56:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/autenticar', async function(req, res){
 })
 
 app.get('/inscrever', async function(req, res){
-    res.render("")
+    res.render("inscrever")
 })
 
 app.post('/logar', async function(req, res){
